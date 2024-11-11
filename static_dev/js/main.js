5d9d3119cf6d8159c2a9c8b45d8398461ca5446a
pablo <pablo.bsti@gmail.com>
2024-11-11 20:04:23 -0300

    configuracion de media queries y botones

--- static_dev/js/main.js
+++ static_dev/js/main.js
@@ -1,5 +1,5 @@
 // GSAP para el efecto de zoom en el carrusel
-gsap.fromTo(".carousel-item img",
+gsap.fromTo("#carouselBanner .carousel-item img",
     { scale: 1.1 }, // Comienza con la imagen aumentada
     { scale: 1, duration: 10, ease: "power1.inOut", repeat: -1, yoyo: true } // Se aleja lentamente
 );
