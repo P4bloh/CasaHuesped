ff4eba0904ca1878661f569a57195d1258ea6815
pablo <pablo.bsti@gmail.com>
2024-11-23 11:36:50 -0300

    Reducir tamaño de imágenes y actualizar carruseles ultimo

--- paginas_estaticas/views.py
+++ paginas_estaticas/views.py
@@ -1,25 +1,39 @@
 import os
+import logging
 from django.conf import settings
 from django.shortcuts import render
 
-def obtener_imagenes_de_carpeta(nombre_carpeta):
-    ruta_imagenes = os.path.join(settings.STATICFILES_DIRS[0], 'img', 'habitaciones', nombre_carpeta)
+logger = logging.getLogger(__name__)
+
+def obtener_imagenes_de_carpeta(ruta_base, nombre_carpeta):
+    ruta_imagenes = os.path.join(settings.STATICFILES_DIRS[0], ruta_base, nombre_carpeta)
     imagenes = []
     if os.path.exists(ruta_imagenes):
-        imagenes = [f'img/habitaciones/{nombre_carpeta}/{img}' for img in os.listdir(ruta_imagenes) if img.endswith('.jpg')]
+        imagenes = [f'{ruta_base}/{nombre_carpeta}/{img}' for img in os.listdir(ruta_imagenes) if img.endswith(('.jpg', 'jpeg'))]
     else:
-        print(f"Error: La ruta {ruta_imagenes} no existe.")
+        logger.error(f"La ruta {ruta_imagenes} no existe.")
     return imagenes
 
 def index(request):
-    imagenes_rock = obtener_imagenes_de_carpeta('rock')
-    imagenes_tango = obtener_imagenes_de_carpeta('tango')
-    imagenes_jazz = obtener_imagenes_de_carpeta('jazz')
+    # Imágenes de habitaciones
+    imagenes_rock = obtener_imagenes_de_carpeta('img/habitaciones', 'rock')
+    imagenes_tango = obtener_imagenes_de_carpeta('img/habitaciones', 'tango')
+    imagenes_jazz = obtener_imagenes_de_carpeta('img/habitaciones', 'jazz')
+    
+    # Imágenes de espacios compartidos
+    imagenes_patio = obtener_imagenes_de_carpeta('img/esp_compartidos', 'patio')
+    imagenes_cocina = obtener_imagenes_de_carpeta('img/esp_compartidos', 'cocina')
+    imagenes_living = obtener_imagenes_de_carpeta('img/esp_compartidos', 'living')
+    imagenes_barrio = obtener_imagenes_de_carpeta('img/esp_compartidos', 'barrio')
     
     params = {
         'imagenes_rock': imagenes_rock,
         'imagenes_tango': imagenes_tango,
         'imagenes_jazz': imagenes_jazz,
+        'imagenes_patio': imagenes_patio,
+        'imagenes_cocina': imagenes_cocina,
+        'imagenes_living': imagenes_living,
+        'imagenes_barrio': imagenes_barrio,
     }
     
     return render(request, 'home/index.html', params)
